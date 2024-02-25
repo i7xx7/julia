f55c9a1c2a6d3b1e1705c089ef071a13eebbb736
i7xx7 <153341323+i7xx7@users.noreply.github.com>
2024-02-25 04:35:39 +0000

    Pending changes exported from your codespace

---
+++ src/commands/eco/coins.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('coins')
+		.setDescription('Veja seus coins atuais'),
+	async execute(interaction, userData) {
+		const { coins } = userData;
+		user = interaction.user
+		console.log(userData.daily)
+		const reply = {
+			color: 16777215,
+			description: `${user}, você possui atualmente ${coins} coins`,
+			timestamp: new Date().toISOString(),
+	        footer: {
+		        text: `${user.username} (${user.id})`,
+		        icon_url: `${user.displayAvatarURL({ extension: 'jpg' })}`,
+	        },
+		}
+		await interaction.reply({ embeds: [reply], ephemeral: true  })
+}};
